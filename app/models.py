debe4e69cb7f43aed8cee213d92e7b7f46a1b736
len3 <dev@mba.home>
2014-09-06 13:28:09 -0400

    Initial commit
    initial commit

---
+++ app/models.py
@@ -0,0 +1,58 @@
+# models.py
+from hashlib import md5
+from app import db
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(255))
+    last_seen = db.Column(db.DateTime)
+    
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
+
+    @staticmethod
+    def make_unique_nickname(nickname):
+        # look up to see if the nickname exists
+        if User.query.filter_by(nickname=nickname) is None:
+            return nickname
+        suffix = 2
+        while True:
+            new_nickname = nickname + str(suffix)
+            if User.query.filter_by(nickname=new_nickname).first() is None:
+                break
+            suffix += 1
+        return new_nickname
+        # if so, return a unique one
+        # if not, return nickname
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
